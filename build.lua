010339413a6b76b9e48d5645cdcfdd30e2fd136f
Joseph Wright <joseph.wright@morningstar2.co.uk>
2016-03-10 21:52:32 +0000

    Switch to l3build scripting

---
+++ build.lua
@@ -0,0 +1,50 @@
+#!/usr/bin/env texlua
+
+-- Build script for "biblatex-phys" files
+
+-- Identify the bundle and module
+bundle = ""
+module = "biblatex-phys"
+
+-- Documentation consists of .tex files to typeset plus .bib file to support
+-- This needs a little bit of trickery to get the .bib file into the right
+-- places
+demofiles    = {"*.bib"}
+typesetfiles = {"*.tex"}
+supportdir       = "."
+typesetsuppfiles = {"*.bib"}
+
+-- .Nothing to unpack
+unpackfiles = { }
+
+-- Install biblatex style files and use these as the sources
+installfiles = {"*.cbx", "*.bbx"}
+sourcefiles  = installfiles
+
+-- Release a TDS-style zip
+packtdszip  = true
+
+-- No tests for this bundle
+testfildir = ""
+
+versionfiles = {"*.bbx", "*.cbx", "*.tex"}
+
+function setversion_update_line (line, date, version)
+  local date = string.gsub(date, "%-", "/")
+  if string.match(line, "Released %d%d%d%d/%d%d/%d%d") or
+     string.match(line, "last revised %d%d%d%d/%d%d/%d%d") then
+    line = string.gsub(line, "%d%d%d%d/%d%d/%d%d", date)
+  end
+  if string.match(line, "This file describes v") then
+    line = string.gsub(line, "v%d%.%d%w?", "v" .. version)
+  end
+  if string.match(line, "^\\ProvidesFile") then
+    line = string.gsub(line, "%d%d%d%d/%d%d/%d%d", date)
+    line = string.gsub(line, "v%d%.%d%w?", "v" .. version)
+  end
+  return line
+end
+
+-- Find and run the build system
+kpse.set_program_name ("kpsewhich")
+dofile (kpse.lookup ("l3build.lua"))
